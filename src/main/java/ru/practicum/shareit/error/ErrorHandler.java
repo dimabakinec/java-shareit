ec68768ad0744d529163f0a1a2387f95fd5108c1
Dima Bakinec <exwanted@gmail.com>
2023-05-22 10:54:18 +0300

    fix: encoding correction

--- src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -17,36 +17,36 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handle(final ItemForbiddenException e) {
-        return new ErrorResponse("?????? ??????? ? ????????", e.getMessage());
+        return new ErrorResponse("Ошибка доступа к предмету", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handle(final ItemValidationException e) {
-        return new ErrorResponse("?????? ????????? ????????", e.getMessage());
+        return new ErrorResponse("Ошибка валидации предмета", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handle(final UserValidationException e) {
-        return new ErrorResponse("?????? ????????? ????????????", e.getMessage());
+        return new ErrorResponse("Ошибка валидации пользователя", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handle(final UserConflictException e) {
-        return new ErrorResponse("???????? ?????? ????????????", e.getMessage());
+        return new ErrorResponse("Конфликт данных пользователя", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handle(final UserNotFoundException e) {
-        return new ErrorResponse("?????? ?????? ????????????", e.getMessage());
+        return new ErrorResponse("Ошибка поиска пользователя", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handle(final ItemNotFoundException e) {
-        return new ErrorResponse("?????? ?????? ????????", e.getMessage());
+        return new ErrorResponse("Ошибка поиска предмета", e.getMessage());
     }
 }
