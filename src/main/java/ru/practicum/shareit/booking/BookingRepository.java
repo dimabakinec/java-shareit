850f8053d439c6f2c8be3c2a256fc2f5303ca1ae
Dima Bakinec <exwanted@gmail.com>
2023-06-05 11:58:42 +0300

    Sprint 14: reviewer's remarks

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -12,37 +12,19 @@ import java.util.*;
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     Optional<Booking> findByIdAndItemOwnerId(Long id, Long ownerId);
 
-    @Query("Select distinct b " +
-            "From Booking AS b " +
-            "join b.item AS i " +
-            "Where b.id = ?1 " +
-            "and (b.booker.id = ?2 or i.owner.id = ?2)")
+    @Query("Select distinct b From Booking AS b join b.item AS i Where b.id = ?1 and (b.booker.id = ?2 or i.owner.id = ?2)")
     Optional<Booking> findByIdAndBookerIdOrItemOwnerId(Long id, Long bookerId);
 
-    @Query("Select b " +
-            "From Booking AS b " +
-            "Where b.booker.id = ?1 " +
-            "and b.start < ?2 " +
-            "and b.end > ?2 " +
-            "ORDER BY b.start ASC")
+    @Query("Select b From Booking AS b Where b.booker.id = ?1 and b.start < ?2 and b.end > ?2 ORDER BY b.start ASC")
     List<Booking> findByBookerIdAndCurrentMomentBetweenStartAndEnd(Long bookerId, LocalDateTime currentMoment);
 
-    @Query("Select b " +
-            "From Booking AS b " +
-            "join b.item AS i " +
-            "Where i.owner.id = ?1 " +
-            "and b.start < ?2 " +
-            "and b.end > ?2 " +
-            "ORDER BY b.start DESC")
+    @Query("Select b From Booking AS b join b.item AS i Where i.owner.id = ?1 and b.start < ?2 and b.end > ?2 ORDER BY b.start DESC")
     List<Booking> findByItemOwnerIdAndCurrentMomentBetweenStartAndEnd(Long ownerId, LocalDateTime currentMoment);
 
     List<Booking> findByItemIdAndBookerIdAndStatusAndEndBefore(Long id, Long bookerId, BookingStatus status,
                                                                  LocalDateTime createdDate);
 
-    @Query("Select b " +
-            "From Booking AS b " +
-            "Where b.item.id = ?1 " +
-            "and (b.start >= ?2 and b.end <= ?3)")
+    @Query("Select b From Booking AS b Where b.item.id = ?1 and (b.start >= ?2 and b.end <= ?3)")
     List<Booking> getBookingDate(Long id, LocalDateTime startDate, LocalDateTime endDate);
 
     List<Booking> findByBookerId(Long userId, Sort sort);
