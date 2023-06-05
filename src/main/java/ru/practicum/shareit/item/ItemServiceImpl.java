124e55fa5b53248ddffde6fc6cae576f4eb66604
Dima Bakinec <exwanted@gmail.com>
2023-06-05 12:57:40 +0300

    Sprint 14: getItem() fixed to Collections.emptyList()

--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -78,12 +78,10 @@ public class ItemServiceImpl implements ItemService {
     public ItemInfo getItem(long userId, long itemId) {
         Item item = itemRepository.findById(itemId)
                 .orElseThrow(() -> new NotFoundException(MODEL_NOT_FOUND.getMessage() + itemId));
-        List<Booking> bookingList = new ArrayList<>();
-
+        List<Booking> bookingList = Collections.<Booking>emptyList();
         if (item.getOwner().getId() == userId) {
             bookingList = bookingRepository.findByItemIdAndStatus(itemId,
                     BookingStatus.APPROVED, Sort.by(Sort.Direction.ASC, "start"));
-
         }
 
         List<Comment> comments = commentRepository.findByItemIdOrderByCreatedAsc(itemId);
