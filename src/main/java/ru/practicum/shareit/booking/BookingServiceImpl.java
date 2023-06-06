dce05a01ceacc032f8a15b316e2c960bb450c5e0
Dima Bakinec <exwanted@gmail.com>
2023-06-06 10:20:21 +0300

    Sprint 14: removed List

--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -164,11 +164,14 @@ public class BookingServiceImpl implements BookingService {
         if (bookingDtoRequest.getEnd().isEqual(bookingDtoRequest.getStart())) {
             throw new ValidationException(INVALID_DATE.getMessage());
         }
-        List<Booking> bookingList = bookingRepository.getBookingDate(bookingDtoRequest.getItemId(),
-                bookingDtoRequest.getStart(), bookingDtoRequest.getEnd());
-        if (bookingList.size() > 0) {
-            throw new ValidationException(INVALID_DATE.getMessage());
-        }
-
+//        List<Booking> bookingList = bookingRepository.getBookingDate(bookingDtoRequest.getItemId(),
+//                bookingDtoRequest.getStart(), bookingDtoRequest.getEnd());
+//        if (bookingList.size() > 0) {
+//            throw new ValidationException(INVALID_DATE.getMessage());
+//        }
+//        Boolean isExists = bookingRepository.getBookingDate(bookingDtoRequest.getItemId(), bookingDtoRequest.getStart(), bookingDtoRequest.getEnd());
+//        if (isExists) {
+//            throw new ValidationException(INVALID_DATE.getMessage());
+//        }
     }
 }
